0de3f7f883880077d5e692eb85873a30f7fa357e
Rax <Rax@DESKTOP-41U8OV7>
2024-06-05 18:07:29 +0300

    [Calc.Domain] Implement calculation service

---
+++ Calculator.Domain/Services/Implementations/CalculationService.cs
@@ -0,0 +1,25 @@
+﻿using Calculator.Domain.Entities;
+using Calculator.Domain.Services.Interfaces;
+using Calculator.Domain.ValueObjects;
+
+namespace Calculator.Domain.Services.Implementations
+{
+    public class CalculationService : ICalculationService
+    {
+        public CalculationResult PerformCalculation(Calculation calculation)
+        {
+            double result = calculation.Operation switch
+            {
+                OperationType.Addition => calculation.Operand1 + calculation.Operand2,
+                OperationType.Subtraction => calculation.Operand1 - calculation.Operand2,
+                OperationType.Multiplication => calculation.Operand1 * calculation.Operand2,
+                OperationType.Division => calculation.Operand2 != 0 ? calculation.Operand1 / calculation.Operand2 : throw new DivideByZeroException(),
+                _ => throw new InvalidOperationException("Invalid operator")
+            };
+
+            calculation.SetResult(result);
+
+            return new CalculationResult(result);
+        }
+    }
+}
