1b2f50104fc55025ca5399dda7fc8207abf7a5d5
Rax <Rax@DESKTOP-41U8OV7>
2024-06-06 01:14:33 +0300

    [Calc.API] Add summary for calc controller

--- Calculator.API/Controllers/CalculatorController.cs
+++ Calculator.API/Controllers/CalculatorController.cs
@@ -14,6 +14,18 @@ namespace Calculator.API.Controllers
             _calculationApplicationService = calculationApplicationService;
         }
 
+        /// <summary>
+        /// Performs arithmetic operation with numbers 
+        /// </summary>
+        /// <remake>
+        /// Sample request:
+        /// Get /calculate?operand1=1&operand2=2&operation=+
+        /// </remake>
+        /// <param name="operand1">1</param>
+        /// <param name="operand2">2</param>
+        /// <param name="operation">+</param>
+        /// <returns>Return 3</returns>
+        /// <response code="200">Success</response>
         [HttpGet("calculate")]
         public IActionResult Calculate(double operand1, double operand2, string operation)
         {
