0d2892e94d8d754954d1bbb41f15e1e73ab02d19
Rax <Rax@DESKTOP-41U8OV7>
2024-06-05 18:16:55 +0300

    [Calc.Application] Add ICalculationAppService and implement it

---
+++ Calculator.Application/Implementations/CalculationApplicationService.cs
@@ -0,0 +1,32 @@
+﻿using Calculator.Application.Interfaces;
+using Calculator.Domain.Entities;
+using Calculator.Domain.Services.Interfaces;
+
+namespace Calculator.Application.Implementations
+{
+    public class CalculationApplicationService : ICalculationApplicationService
+    {
+        private readonly ICalculationService _calculationService;
+
+        public CalculationApplicationService(ICalculationService calculationService)
+        {
+            _calculationService = calculationService;
+        }
+
+        public double Calculate(double operand1, double operand2, string operation)
+        {
+            var operationType = operation switch
+            {
+                "+" => OperationType.Addition,
+                "-" => OperationType.Subtraction,
+                "*" => OperationType.Multiplication,
+                "/" => OperationType.Division,
+                _ => throw new InvalidOperationException("Invalid operator")
+            };
+
+            var calculation = new Calculation(operand1, operand2, operationType);
+            var result = _calculationService.PerformCalculation(calculation);
+            return result.Value;
+        }
+    }
+}
---
+++ Calculator.Application/Interfaces/ICalculationApplicationService.cs
@@ -0,0 +1,7 @@
+﻿namespace Calculator.Application.Interfaces
+{
+    public interface ICalculationApplicationService
+    {
+        double Calculate(double operand1, double operand2, string operation);
+    }
+}
