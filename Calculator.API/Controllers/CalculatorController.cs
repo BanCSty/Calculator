16ead8ab8365eacf4b865c9445678b016c5752db
Rax <Rax@DESKTOP-41U8OV7>
2024-06-05 18:28:10 +0300

    [Calc.API] Add [Calculator] controller

---
+++ Calculator.API/Controllers/CalculatorController.cs
@@ -0,0 +1,31 @@
+﻿using Calculator.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Calculator.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ICalculationApplicationService _calculationApplicationService;
+
+        public CalculatorController(ICalculationApplicationService calculationApplicationService)
+        {
+            _calculationApplicationService = calculationApplicationService;
+        }
+
+        [HttpGet("calculate")]
+        public IActionResult Calculate(double operand1, double operand2, string operation)
+        {
+            try
+            {
+                var result = _calculationApplicationService.Calculate(operand1, operand2, operation);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
