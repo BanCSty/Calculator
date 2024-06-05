64e2b35e34574967bc0c7757f04ff262798cc8b2
Rax <Rax@DESKTOP-41U8OV7>
2024-06-05 18:39:52 +0300

    [Calc.Tests] Add tests for [Domain] services

---
+++ Calculator.Tests/Domain/Services/CalculationServiceTests.cs
@@ -0,0 +1,55 @@
+﻿using Calculator.Domain.Entities;
+using Calculator.Domain.Services.Implementations;
+using Calculator.Domain.Services.Interfaces;
+
+namespace Calculator.Tests.Domain.Services
+{
+    public class CalculationServiceTests
+    {
+        private readonly ICalculationService _calculationService;
+
+        public CalculationServiceTests()
+        {
+            _calculationService = new CalculationService();
+        }
+
+        [Fact]
+        public void PerformCalculation_Addition_ReturnsCorrectResult()
+        {
+            var calculation = new Calculation(1, 2, OperationType.Addition);
+            var result = _calculationService.PerformCalculation(calculation);
+            Assert.Equal(3, result.Value);
+        }
+
+        [Fact]
+        public void PerformCalculation_Subtraction_ReturnsCorrectResult()
+        {
+            var calculation = new Calculation(5, 3, OperationType.Subtraction);
+            var result = _calculationService.PerformCalculation(calculation);
+            Assert.Equal(2, result.Value);
+        }
+
+        [Fact]
+        public void PerformCalculation_Multiplication_ReturnsCorrectResult()
+        {
+            var calculation = new Calculation(2, 3, OperationType.Multiplication);
+            var result = _calculationService.PerformCalculation(calculation);
+            Assert.Equal(6, result.Value);
+        }
+
+        [Fact]
+        public void PerformCalculation_Division_ReturnsCorrectResult()
+        {
+            var calculation = new Calculation(6, 3, OperationType.Division);
+            var result = _calculationService.PerformCalculation(calculation);
+            Assert.Equal(2, result.Value);
+        }
+
+        [Fact]
+        public void PerformCalculation_DivisionByZero_ThrowsDivideByZeroException()
+        {
+            var calculation = new Calculation(1, 0, OperationType.Division);
+            Assert.Throws<DivideByZeroException>(() => _calculationService.PerformCalculation(calculation));
+        }
+    }
+}
